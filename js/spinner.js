8ea8e3d53c706525cef95eebbd80069ba4dca497
Alexandr <a.landar@doctis.ru>
2021-11-14 03:31:57 +0300

    feat(App): some updates in function

--- js/spinner.js
+++ js/spinner.js
@@ -3,6 +3,7 @@ window.onload = function () {
   window.setTimeout(function () {
     document.body.classList.add('loaded');
     document.body.classList.remove('loaded_hiding');
+    cookies.classList.add('cookies-active');
   }, 500);
 }
 
